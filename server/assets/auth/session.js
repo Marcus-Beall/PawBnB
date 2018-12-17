ec9ef9658e31c8100087cd4357f582d72cb7f4c0
Marcus Beall <marcbeall@gmail.com>
2018-12-17 15:27:43 -0700

    more server stuff

---
+++ server/assets/auth/session.js
@@ -0,0 +1,24 @@
+var expressSession = require("express-session");
+var mongoStore = require("connect-mongodb-session")(expressSession);
+
+var store = new mongoStore({
+  uri: "mongodb://student:student1@ds016298.mlab.com:16298/kanbanboard", //CHANGE ME!!!!!!
+  collection: "Sessions"
+});
+
+store.on("error", function (err) {
+  console.log("[SESSION ERROR]", err);
+});
+
+// @ts-ignore
+var session = expressSession({
+  secret: "you should change this", //CHANGE ME!!!!
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+  store,
+  resave: true,
+  saveUninitialized: true
+});
+
+module.exports = session;

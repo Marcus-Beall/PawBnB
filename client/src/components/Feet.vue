e227ac81c089f3b0f18081e17ada90d043bddb92
Ari Ajir <mitra.ariana@gmail.com>
2018-12-18 09:58:07 -0700

    hyperlinked icons to social media

--- client/src/components/Feet.vue
+++ client/src/components/Feet.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="feet">
-    <i class="icon fab fa-facebook-square"></i>
-    <i class="fab fa-twitter-square"></i>
-    <i class="fab fa-instagram"></i>
-    <i class="fab fa-snapchat-square"></i>
+    <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+    <a href="https://twitter.com/login?lang=en" target="_blank"><i class="fab fa-twitter-square"></i></a>
+    <a href="https://www.instagram.com/accounts/login/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+    <a href="https://accounts.snapchat.com/accounts/login?continue=https%3A%2F%2Faccounts.snapchat.com%2Faccounts%2Fwelcome"
+      target="_blank"><i class="fab fa-snapchat-square"></i></a>
   </div>
 </template>
 
@@ -22,6 +23,10 @@
 </script>
 
 <style>
+  a {
+    color: #231f20;
+  }
+
   .fab {
     font-size: 30px;
     padding-left: 5px;
@@ -31,6 +36,21 @@
 
   .fa-facebook-square:hover {
     color: #4267b2;
+    transition-delay: .02s;
+  }
+
+  .fa-twitter-square:hover {
+    color: #38a1f3;
+    transition-delay: .02s;
+  }
+
+  .fa-instagram:hover {
+    color: #e4405f;
+    transition-delay: .02s;
+  }
+
+  .fa-snapchat-square:hover {
+    color: #fffc00;
     transition-delay: .02s;
   }
 </style>
